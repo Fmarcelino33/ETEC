158b4f7aae27a03464b4647e0b96f868bf8475d8
Francisco Marcelino <fmarcelino26@gmail.com>
2026-08-26 20:28:21 -0300

    Add files via upload

---
+++ programa_e_algoritmo/ex10.js
@@ -0,0 +1,14 @@
+//EXERCICIO #4
+const input = require("readline-sync");
+
+let n1 = input.questionInt("Digite um numero: ");
+
+let var1 = n1 / 2;
+let var2 = n1 /2;
+
+var1 = var1 + 3;
+var2 = var2 + 5;
+
+console.log(`Var1 = ${var1} e Var2 = ${var2}`
+    
+);
