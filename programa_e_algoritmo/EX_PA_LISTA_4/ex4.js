e9760f65b958132cf49dc3f8877fb12e40d55b94
Francisco Marcelino <fmarcelino26@gmail.com>
2026-09-09 11:06:54 -0300

    Update ex4.js

--- programa_e_algoritmo/EX_PA_LISTA_4/ex4.js
+++ programa_e_algoritmo/EX_PA_LISTA_4/ex4.js
@@ -1,23 +1,17 @@
-
 const input = require("readline-sync");
 
 let n1 = input.questionInt("Digite n1: ");
 let n2 = input.questionInt("Digite n2: ");
 let n3 = input.questionInt("Digite n3: ");
 
-let maiorvalor = (n1 > n2 == n3)
+let maior = n1;
 
-if (n1 > n2) {
-    if (n1 > n3) {
-        console.log(n1, "maior valor");
-    } else {
-        console.log(n2, "maior valor");
-    }
-    if (n3 < n2) {
-        if (n3 < n2) {
-            console.log(n2, "menor valor");
-        } else {
-            console.log(n3, "menor valor");
-    }
-}    
+if (n2 > maior) {
+  maior = n2;
 }
+if (n3 > maior) {
+  maior = n3;
+}
+
+console.log("O maior numero e: ", maior);
+
