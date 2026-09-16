e69a8af926a9cfc7fcd78857e13e0605b757dbb3
Francisco Marcelino <fmarcelino26@gmail.com>
2026-09-16 20:21:38 -0300

    Remove commented code from exe1.js

--- programa_e_algoritmo/EX_LISTA_6/exe1.js
+++ programa_e_algoritmo/EX_LISTA_6/exe1.js
@@ -18,20 +18,3 @@ while (numero <= 20) {
 
 
 
-
-// EXERCICIO TABUADA//
-// let tabuada = 1;
-// while (tabuada <= 10) {
-
-//   console.log(`######### TABUADA DO ${tabuada} ########`);
-    
-//   let multiplicando = 1;
-//   while (multiplicando <= 10) {
-//     console.log(`${tabuada} x ${multiplicando} = ${tabuada * multiplicando}`)
-//     multiplicando = multiplicando + 1;
-//   }
-    
-//   console.log(`#############################`); 
-  
-//     tabuada = tabuada + 1;
-// }
